f09db2fe2d001c74ddcd6ce55e83375915f7147c
sjy9412 <sjy9412@gmail.com>
2019-11-19 13:56:57 +0900

    Django | Add user detail

--- todo-django/todos/views.py
+++ todo-django/todos/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.contrib.auth import get_user_model
 
 from .models import Todo
-from .serializers import TodoSerializers
+from .serializers import TodoSerializers, UserSerializers
 
 # Create your views here.
 
@@ -22,3 +23,10 @@ def todo_index_create(request):
         if serializers.is_valid(raise_exception=True):
             serializers.save()
             return Response(serializers.data)
+
+@api_view(['GET'])
+def user_detail(request, id):
+    User = get_user_model()
+    user = get_object_or_404(User, pk=id)
+    serializers = UserSerializers(user)
+    return Response(serializers.data)
